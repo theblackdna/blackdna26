dba73ef614830753fc824c16be6eac99f2bcad13
DNA <38492909+theblackdna@users.noreply.github.com>
2023-02-27 18:33:48 +0800

    Update main.js

--- _scripts/main.js
+++ _scripts/main.js
@@ -72,6 +72,7 @@ $(function() {
   sr.reveal('.background');
   sr.reveal('.skills');
   sr.reveal('.experience', { viewFactor: 0.2 });
+  sr.reveal('.education', { viewFactor: 0.2 });
   sr.reveal('.featured-projects', { viewFactor: 0.1 });
   sr.reveal('.other-projects', { viewFactor: 0.05 });
 });
